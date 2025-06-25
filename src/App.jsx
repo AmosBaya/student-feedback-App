781a3a03e6c334214f43e347b0942816e9a50b24
Amos Baya <amosbaya96@gmail.com>
2025-06-25 13:45:24 +0300

    done initializing and creating some files in my project

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import FeedbackForm from "@/components/FeedbackForm";
+import FeedbackList from "@/components/FeedbackList";
+
+export default function App(){
+  const [feedbacks, setFeedbacks] 
+}
