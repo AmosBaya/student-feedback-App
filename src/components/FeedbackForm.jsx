f5745b52d59172676397dae4100d0a4f9d42ca13
Amos Baya <amosbaya96@gmail.com>
2025-06-25 15:01:44 +0300

    done creating the feedback form

---
+++ src/components/FeedbackForm.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { Input } from "@components/ui/input";
+import { Textarea } from "@components/ui/textarea";
+import { Button } from "@components/ui/button";
+
+export default function FeedbackForm({onAdd}) {
+    const [ name, setName ] = useState("");
+    const [ comment, setComment ] = useState("");
+    const [ rating, setRating ] = useState("");
+
+
+    const hundleSubmit = (e) => {
+        e.preventDefault();
+        if (!name.trim() || !comment.trim() || !rating.trim()) return; // return null if there is no input
+
+        onAdd({ name, comment, rating }); // taking in props or parsing data
+        // updating
+        setName("");
+        setComment("");
+        setRating("")
+    };
+
+    return(
+        <form className="space-y-4 " onSubmit={hundleSubmit}>
+            <Input 
+                placeholder="Student Name...." 
+                value={name}
+                onChange={e => setName(e.target.value)}
+            />
+
+            <Textarea 
+                placeholder="Feedback"
+                value={comment}
+                onChange={e => setComment(e.target.value)}
+            />
+
+            <Input
+                placeholder="Rating (1-5)"
+                type="number"
+                value={rating}
+                onChange={e => setRating(e.target.value)}
+            />
+
+            <Button type="submit">Submit Feedback</Button>
+        </form>
+    )
+}
